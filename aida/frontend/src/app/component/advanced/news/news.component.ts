5058729978e31a8945b9a67b295bc09783d57c55
aleFioreC <alessio.fiore1995@libero.it>
2023-03-03 17:10:29 +0100

    varie fix

--- aida/frontend/src/app/component/advanced/news/news.component.ts
+++ aida/frontend/src/app/component/advanced/news/news.component.ts
@@ -7,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  cards = [];
-
-  imageSource;
-
   constructor() { }
 
   ngOnInit(): void {
